0e8235e8395d98fcd61b15f1ddce30e69c797d79
xiaodong chen <cxd032404@hotmail.com>
2021-02-09 16:43:49 +0800

    merge

--- function/config_master.php
+++ function/config_master.php
@@ -11,6 +11,6 @@ $base_config = [
 
 $additional_config = [
     'site_description'=> $base_config['site_name'].'致力于服务广大'.$base_config['game_name'].'玩家，为'.$base_config['game_name'].'玩家提供丰富的'.$base_config['game_name'].'游戏攻略、'.$base_config['game_name'].'电子竞技赛事资讯、数据分析及内容解读。',
-    'api_get' => $base_config['api_url']."/lol/get"
+    'api_get' => $base_config['api_url']."/get"
 ];
 return array_merge($base_config,$additional_config);
