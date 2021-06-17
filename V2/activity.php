bc3b83a77aab244fa9f5a7dcc4df2efe6ebc975b
justin.chen <cxd032404@hotmail.com>
2021-06-17 18:49:09 +0800

    merge

--- V2/activity.php
+++ V2/activity.php
@@ -3,6 +3,7 @@ require_once "function/init.php";
 $data = [
     "links"=>["page"=>1,"page_size"=>6,"site_id"=>$config['site_id']],
     "defaultConfig"=>["keys"=>["wechat_qr_code","QQ","site_desc","introduction","aboutus","contact","weibo"],"site_id"=>$config['site_id']],
+    "activityList"=>["page"=>1,"page_size"=>20,"site_id"=>$config['site_id']]
 ];
 $return = curl_post($config['api_get'],json_encode($data),1);
 ?>
@@ -28,53 +29,21 @@ $return = curl_post($config['api_get'],json_encode($data),1);
     </header>
     <div class="activity_all width">
         <div class="activity">
-            <a href="##">
-                <div class="activity_img">
-                    <img src="<?php echo $config['site_url'];?>/images/activity.png" alt="">
-                </div>
-                <div class="activity_mid">
-                    <p class="activity_title">充值大放送！新版首发·巨额回馈</p>
-                    <p class="activity_time">活动时间：6.16—6.18</p>
-                </div>
-                <div class="activity_description">
-                    夏季赛常规赛上海主场售票将于6月2日 14 ：00点正式开启6月7日-6月13日门票的售卖 ，此后每周周一固定开启下一周的门票售卖。除上海LPL主场外，深圳、苏州、杭州的俱乐部主场也将在赛季期间陆续开放售票，召唤师们可通过官方售票渠道进行购票。<br>
-                    同时，上海主场将开放冠军战队VIP Pass试运营，并将于6月1日 14 ：00点开启售卖。购买战队季票的观众可观看本赛季常规赛（6月7日-8月8日）上海主场啊夏季赛常规赛上海主场售票将于6月2日 14 ：00点正式开启6月7日-6月13日门票的售卖 ，此后每周周一固定开启下一周的门票售卖。除上海LPL主场外，深圳、苏州、杭州的俱乐部主场也将在赛季期间陆续开放售票，召唤师们可通过官方售票渠道进行购票。
-                    同时，上海主场将开放冠军战队VIP Pass试运营，并将于6月1日 14 ：00点开启售卖。购买战队季票的观众可观看本赛季常规赛（6月7日-8月8日）上海主场啊...
-                </div>
-            </a>
+            <?php foreach($return['activityList']['data'] as $activeInfo){?>
+                <a href="<?php echo $activeInfo['url'];?>" target = "_blank">
+                    <div class="activity_img">
+                        <img src="<?php echo $activeInfo['logo'];?>" alt="<?php echo $activeInfo['title'];?>">
+                    </div>
+                    <div class="activity_mid">
+                        <p class="activity_title"><?php echo $activeInfo['title'];?></p>
+                        <p class="activity_time">活动时间：<?php echo date("m.d",$activeInfo['start_time']);?>—<?php echo date("m.d",$activeInfo['end_time']);?></p>
+                    </div>
+                    <div class="activity_description">
+                        <?php echo html_entity_decode($activeInfo['description']);?>
+                    </div>
+                </a>
         </div>
-        <div class="activity">
-            <a href="##">
-                <div class="activity_img">
-                    <img src="<?php echo $config['site_url'];?>/images/activity1.png" alt="">
-                </div>
-                <div class="activity_mid">
-                    <p class="activity_title">充值大放送！新版首发·巨额回馈</p>
-                    <p class="activity_time">活动时间：6.16—6.18</p>
-                </div>
-                <div class="activity_description">
-                    夏季赛常规赛上海主场售票将于6月2日 14 ：00点正式开启6月7日-6月13日门票的售卖 ，此后每周周一固定开启下一周的门票售卖。除上海LPL主场外，深圳、苏州、杭州的俱乐部主场也将在赛季期间陆续开放售票，召唤师们可通过官方售票渠道进行购票。<br>
-                    同时，上海主场将开放冠军战队VIP Pass试运营，并将于6月1日 14 ：00点开启售卖。购买战队季票的观众可观看本赛季常规赛（6月7日-8月8日）上海主场啊夏季赛常规赛上海主场售票将于6月2日 14 ：00点正式开启6月7日-6月13日门票的售卖 ，此后每周周一固定开启下一周的门票售卖。除上海LPL主场外，深圳、苏州、杭州的俱乐部主场也将在赛季期间陆续开放售票，召唤师们可通过官方售票渠道进行购票。
-                    同时，上海主场将开放冠军战队VIP Pass试运营，并将于6月1日 14 ：00点开启售卖。购买战队季票的观众可观看本赛季常规赛（6月7日-8月8日）上海主场啊...
-                </div>
-            </a>
-        </div>
-        <div class="activity">
-            <a href="##">
-                <div class="activity_img">
-                    <img src="<?php echo $config['site_url'];?>/images/activity1.png" alt="">
-                </div>
-                <div class="activity_mid">
-                    <p class="activity_title">充值大放送！新版首发·巨额回馈</p>
-                    <p class="activity_time">活动时间：6.16—6.18</p>
-                </div>
-                <div class="activity_description">
-                    夏季赛常规赛上海主场售票将于6月2日 14 ：00点正式开启6月7日-6月13日门票的售卖 ，此后每周周一固定开启下一周的门票售卖。除上海LPL主场外，深圳、苏州、杭州的俱乐部主场也将在赛季期间陆续开放售票，召唤师们可通过官方售票渠道进行购票。<br>
-                    同时，上海主场将开放冠军战队VIP Pass试运营，并将于6月1日 14 ：00点开启售卖。购买战队季票的观众可观看本赛季常规赛（6月7日-8月8日）上海主场啊夏季赛常规赛上海主场售票将于6月2日 14 ：00点正式开启6月7日-6月13日门票的售卖 ，此后每周周一固定开启下一周的门票售卖。除上海LPL主场外，深圳、苏州、杭州的俱乐部主场也将在赛季期间陆续开放售票，召唤师们可通过官方售票渠道进行购票。
-                    同时，上海主场将开放冠军战队VIP Pass试运营，并将于6月1日 14 ：00点开启售卖。购买战队季票的观众可观看本赛季常规赛（6月7日-8月8日）上海主场啊...
-                </div>
-            </a>
-        </div>
+            <?php }?>
     </div>
     <div class="foot">
         <div class="width">
---
+++ V2/detail.php
@@ -0,0 +1,122 @@
+<?php
+require_once "function/init.php";
+$id = $_GET['id']??1;
+$data = [
+    "information"=>[$id],
+    "recentInformationList"=>["dataType"=>"informationList","page"=>1,"site"=>1,"page_size"=>6,"type"=>"1,2,3,5,6,7","fields"=>"*","except_id"=>$id],
+];
+$return = curl_post($config['api_get'],json_encode($data),1);
+if(isset($return["information"]['data']['redirect']) && $return["information"]['data']['redirect']>0)
+{
+    renderDetail301($config,$return["information"]['data']['redirect']);
+}
+if(!isset($return["information"]['data']['id'])){
+    render404($return['information']['data'],$config);//404跳转
+}
+$return["information"]['data']['baidu_word_list'] = json_decode($return["information"]['data']['baidu_word_list'],true)??[];
+$ids = array_keys($return["information"]['data']['baidu_word_list']);
+$ids = count($ids)>0?implode(",",$ids):"0";
+$data2 =
+    [
+            "ConnectInformationList"=>["dataType"=>"baiduInformaitonList","site"=>1,"ids"=>$ids,"page"=>1,"page_size"=>6,"type"=>"1,2,3,5,6,7","fields"=>"id,title,site_time","expect_id"=>$id],
+    ];
+$return2 = curl_post($config['api_get'],json_encode($data2),1);
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $return['information']['data']['title'];?>-<?php echo $config['site_name'];?></title>
+    <?php renderHeaderJsCss($config,[]);?>
+</head>
+
+<body class="newsList_body">
+    <header>
+        <ul class="head width">
+            <?php generateNav($config,"news");?>
+        </ul>
+    </header>
+    <div class="newsDetail">
+        <div class="width">
+            <div class="newsDetail_top">
+                <p><?php echo $return['information']['data']['title'];?></p>
+                <span><?php echo date("Y-m-d H:i",strtotime($return['information']['data']['create_time']));?></span>
+            </div>
+            <div class="newsDetail_content">
+                <?php echo html_entity_decode($return['information']['data']['content']);?>
+                <img src="<?php echo $return['information']['data']['logo'];?>" alt="<?php echo $return['information']['data']['title'];?>">
+            </div>
+            <div class="newsDetail_label">
+                <?php $i=0;if(count($return["information"]['data']['baidu_word_list'])>0){ foreach($return["information"]['data']['baidu_word_list'] as $key => $word){?>
+                    <span><?php echo $word['tag'];?></span>
+                    <?php $i++;if($i==3){break;}}}?>
+            </div>
+        </div>
+    </div>
+    <div class="width news_all">
+        <?php if(count($return2['ConnectInformationList']['data'])>0){?>
+            <div class="relevant_news">
+                <div class="news_title">
+                    <img src="<?php echo $config['site_url'];?>/images/news_ico.png" alt="">
+                    <span>相关文章推荐</span>
+                </div>
+                <ul class="clearfix">
+                    <?php foreach($return2['ConnectInformationList']['data'] as $info){?>
+                        <li>
+                            <a href="<?php echo $config['site_url'];?>/detail/<?php echo $info['id'];?>">
+                                <?php echo $info['title'];?>
+                            </a>
+                        </li>
+                    <?php }?>
+                </ul>
+            </div>
+        <?php }?>
+        <div class="latest_news">
+            <div class="news_title">
+                <img src="<?php echo $config['site_url'];?>/images/news_ico.png" alt="">
+                <span>最新资讯</span>
+            </div>
+            <ul class="clearfix">
+                <?php foreach($return['recentInformationList']['data'] as $info){?>
+                    <li>
+                        <a href="<?php echo $config['site_url'];?>/detail/<?php echo $info['id'];?>">
+                            <?php echo $info['title'];?>
+                        </a>
+                    </li>
+                <?php }?>
+            </ul>
+        </div>
+    </div>
+    <div class="foot">
+        <div class="width">
+            <p class="link_title">友情链接</p>
+            <ul class="foot_ul">
+                <?php
+                foreach($return['links']['data'] as $linksInfo)
+                {   ?>
+                    <li><a href="<?php echo $linksInfo['url'];?>"><?php echo $linksInfo['name'];?></a></li>
+                <?php }?>
+            </ul>
+            <?php renderCertification($config);?>
+        </div>
+    </div>
+    <script>
+        $(function () {
+            //头部header
+            $(window).on('scroll', function () {
+                if ($(this).scrollTop() > 88) {
+                    $('body').addClass('scrolled')
+                } else {
+                    $('body').removeClass('scrolled');
+                }
+
+
+            })
+        })
+    </script>
+</body>
+
+</html>
